76133c104f0c5f505b251e3d9599341650e80be1
nqhuy213 <nqhuy213@gmail.com>
2021-02-06 13:10:03 +1000

    Updates

---
+++ precache-manifest.059834170df30e9f58de38e82cd11030.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf0cf42f2d90bfaf6cfb2b6c9f5e2d6",
+    "url": "/SocialMedia-ReactApp/index.html"
+  },
+  {
+    "revision": "e297a2de5e1c50b9d317",
+    "url": "/SocialMedia-ReactApp/static/css/2.5564b9d4.chunk.css"
+  },
+  {
+    "revision": "54098542e8850176d958",
+    "url": "/SocialMedia-ReactApp/static/css/main.f192fcb0.chunk.css"
+  },
+  {
+    "revision": "e297a2de5e1c50b9d317",
+    "url": "/SocialMedia-ReactApp/static/js/2.38ab6a6d.chunk.js"
+  },
+  {
+    "revision": "29f04ae39f8b027627f4fb59bac003bc",
+    "url": "/SocialMedia-ReactApp/static/js/2.38ab6a6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54098542e8850176d958",
+    "url": "/SocialMedia-ReactApp/static/js/main.4e7745d8.chunk.js"
+  },
+  {
+    "revision": "f2b541ae8d91974185bc",
+    "url": "/SocialMedia-ReactApp/static/js/runtime-main.8ce4c00c.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/SocialMedia-ReactApp/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/SocialMedia-ReactApp/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/SocialMedia-ReactApp/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/SocialMedia-ReactApp/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/SocialMedia-ReactApp/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/SocialMedia-ReactApp/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/SocialMedia-ReactApp/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/SocialMedia-ReactApp/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/SocialMedia-ReactApp/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/SocialMedia-ReactApp/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/SocialMedia-ReactApp/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "43f9ef054637d29d671a737bee6c1111",
+    "url": "/SocialMedia-ReactApp/static/media/logo.43f9ef05.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/SocialMedia-ReactApp/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/SocialMedia-ReactApp/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/SocialMedia-ReactApp/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/SocialMedia-ReactApp/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/SocialMedia-ReactApp/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
